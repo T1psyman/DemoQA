4cb77af5746c548253a91115b99a5372c5bb518f
Egor Blinov <gblinoff@gmail.com>
2023-02-26 13:55:28 +0300

    HW 5 part1 done

--- src/test/java/exercise5/OpenEnterpriseWithHover.java
+++ src/test/java/exercise5/OpenEnterpriseWithHover.java
@@ -1,19 +1,18 @@
 package exercise5;
 
 import com.codeborne.selenide.Condition;
-import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
 
 public class OpenEnterpriseWithHover {
 
     @Test
-    void openedEnterprisesPage(){
+    void openedEnterprisesPage() {
         open("https://github.com/");
         $(".Header-old").$$("ul li").get(14).hover();
         $(".Header-old").$$("ul li").get(16).click();
         $(".font-mktg").shouldHave(Condition.text("Build like the best"));
-        sleep(5000);
     }
 }
